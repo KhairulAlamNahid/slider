bd5cf1403f354eec9b4e3f3f6d5aa05517f15c9c
KhairulAlamNahid <profreelancer10@gmail.com>
2021-03-23 22:32:42 +0600

    update

--- js/script.js
+++ js/script.js
@@ -9,4 +9,14 @@ $(function () {
         nextArrow: '<i class="fa fa-chevron-right right"></i>',
         dots: true,
     });
+    // banner slider js
+
+    $('.banner2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        prevArrow: '<i class="fa fa-chevron-left left"></i>',
+        nextArrow: '<i class="fa fa-chevron-right right"></i>',
+        dots: true,
+    });
 })
